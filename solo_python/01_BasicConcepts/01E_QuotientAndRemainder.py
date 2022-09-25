54d89750d8fe216bb447f3d12c4ddaa446106f43
FoXcodeZ <m.grabowy@int.pl>
2022-09-25 21:01:06 +0200

    added module 01E Quotient And Remainder

---
+++ solo_python/01_BasicConcepts/01E_QuotientAndRemainder.py
@@ -0,0 +1,16 @@
+
+if __name__ == '__main__':
+    print("// operator divide numbers into integers, ignoring the part after the dot,")
+    print("even if the result can be possibly a floating-point number.")
+    print("20 // 6 = 20 / 6 with only integer part of the number = ", 20 // 6)
+    print("4 is 0 times in 3 because is smaller, so 3 // 4 = ", 3 // 4)
+    print("7 is 3 times in 23 (we can't fit 4 sevens), so 23 // 7 = ", 23 // 7)
+
+    print()
+
+    print("The modulo operator is carried out with a percent symbol (%))")
+    print("and is used to get the remainder of a division.")
+    print("Reminder from the division 23 / 7 is ", 23 % 7)
+    print("It's because 23 / 7 = 3 and 2 remainder")
+    print("7 fits 3 times in 23 and 2 is a remainder")
+    print("3 * 7 = 21 and 2 remainder is (3 * 7) + 2 = 23")
